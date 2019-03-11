4cdd9229b1301b807fb4ea438e1f4a216e87a8ec
adithak <adithak59@gmail.com>
2019-03-11 11:05:55 +0530

    Create ff.c

---
+++ ff.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	int m,k;
+	scanf("%d",&m);
+	int a[m];
+	for(i=0;i<m;k++)
+	scanf("%d",&a[k]);
+	for(k=0;i<m;k++)
+	{
+		if(a[k]==k)
+		printf("%d",a[k]);
+    }
+}
